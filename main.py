13b63a185fa36c4f19b4e53085c1dac8c99149cf
91836 <devashish.nigam89@gmail.com>
2021-09-09 13:59:28 +0530

    geeks for geeks explanation

--- main.py
+++ main.py
@@ -1,24 +1,21 @@
 # Insertion sort - similar to way we sort cards
 # put marker after 1st element ==> 9|8,6,4,5,7
-# 8,9|6,4,5,7
+# 8,9|6,4,5,7 # before pipe would be sorted and each element after pipe is inserted into sorted array before pipe
 # 6,8,9|4,5,7
 # 4,6,8,9|5,7
 # 4,5,6,8,9|7
 # 4,5,6,7,8,9|
 
-def swap_list_elements(lst_ele, i, j):
-    lst_ele[i], lst_ele[j] = lst_ele[j], lst_ele[i]
+def insertionSort(lst):
+    n = len(lst)
+    for i in range(1,n):
+        x = lst[i]
+        # ulta loop left of marker(|) to run from i-1 to 0 with steps -1 to
+        # check if new lst[i] can be inserted in sorted array before marker (|)
+        for j in range(i-1, -1, -1):
+            if x < lst[j]:
+                lst[j], lst[j+1] = x, lst[j]
 
-
-def insertionSort(lst_ele):
-    start = 1
-    while start < len(lst_ele):
-        for i in range(start, 0, -1):
-            if lst_ele[i] < lst_ele[i - 1]:
-                swap_list_elements(lst_ele, i, i - 1)
-                # print(lst_ele, start)
-
-        start += 1
 
 
 if __name__ == '__main__':
